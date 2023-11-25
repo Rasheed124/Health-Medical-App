642002e8fa417d9998038e3b65ef4305a6546815
Rasheed124 <rashdevatrealglobe@gmail.com>
2023-11-25 08:58:53 -1100

    Health APP

--- src/components/PatientSidebar.jsx
+++ src/components/PatientSidebar.jsx
@@ -14,7 +14,7 @@ const PatientSidebar = () => {
               href=""
               className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
             >
-              Doctor Dashboard
+              Patient Dashboard
             </a>
           </li>
 
