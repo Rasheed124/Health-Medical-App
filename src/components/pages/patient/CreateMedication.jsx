f505f378d0cb2b288859d1b82b5d4dcfbde93579
Shobola Faruq <66446091+Faruq-Shobola@users.noreply.github.com>
2023-11-27 11:18:32 +0000

    Create the post route to add new medication

--- src/components/pages/patient/CreateMedication.jsx
+++ src/components/pages/patient/CreateMedication.jsx
@@ -1,26 +1,116 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const CreateMedication = () => {
+    const [medicationData, setMedicationData] = useState({
+      name: '',
+      description: '',
+      note: '',
+      startdate: '',
+      enddate: '',
+      timefrequency: [],
+      assignedto: '',
+    });
+  
+    const [patients, setPatients] = useState([]);
+    const [loading, setLoading] = useState(true);
+  
+    // Assuming you have the user token stored in the session
+    const userToken = sessionStorage.getItem('userToken');
+
+    useEffect(() => {
+      // Fetch the list of patients from the API
+      axios.get('YOUR_PATIENTS_API_ENDPOINT', {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      })
+        .then(response => {
+          setPatients(response.data);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.error('Error fetching patients:', error.response ? error.response.data : error.message);
+          setLoading(false);
+        });
+    }, [userToken]);
+
+    const handleChange = (e) => {
+      const { name, value, type, checked } = e.target;
+  
+      if (type === 'checkbox') {
+        // If it's a checkbox, update the state array based on its checked status
+        setMedicationData((prevData) => {
+          if (checked) {
+            return {
+              ...prevData,
+              timefrequency: [...prevData.timefrequency, name],
+            };
+          } else {
+            return {
+              ...prevData,
+              timefrequency: prevData.timefrequency.filter((time) => time !== name),
+            };
+          }
+        });
+      } else {
+        // For other input types, update the state as usual
+        setMedicationData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+      }
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      // Make the POST request with Axios
+      axios.post('https://health-connect-cd7q.onrender.com/api/v1/medication', medicationData, {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => {
+        console.log('Medication added successfully:', response.data);
+        // You may want to handle success or redirect the user here
+      })
+      .catch(error => {
+        console.error('Error adding medication:', error.response ? error.response.data : error.message);
+        // Handle error
+      });
+
+    };
+
+
   return (
     <div className=" w-full">
       <h2 className=" py-4 text-xl text-[#0a7dcf] font-bold">
         {" "}
         Create A Medication
       </h2>
-      <form action=" " className=" max-w-[400px]">
+      <form action={handleSubmit} className=" max-w-[400px]">
         <div className=" w-full">
           <label htmlFor="">Name</label>
           <input
             type="text"
-            placeholder=" Medication Name"
-            className=" w-full border border-black py-2 pl-3 mt-2 rounded-md "
+            id="name"
+            name="name"
+            value={medicationData.name}
+            onChange={handleChange}
+            placeholder="Medication Name"
+            className=" w-full border border-black py-2 pl-3 mt-2 rounded-md"
           />
         </div>
         <div className=" pt-5">
           <label htmlFor="">Prescription</label> <br />
           <textarea
-            name=""
-            id=""
+            type="text"
+            id="description"
+            name="description"
+            value={medicationData.description}
+            onChange={handleChange}
             cols="30"
             rows="4"
             className=" w-full resize-none mt-2 border border-black rounded-md"
@@ -31,38 +121,74 @@ const CreateMedication = () => {
           <div className=" flex w-full gap-4 pt-2">
             <input
               type="date"
+              id="startdate"
+              name="startdate"
+              value={medicationData.startdate}
+              onChange={handleChange}
               className=" w-full py-2 pl-2 rounded-md border border-black"
             />
             <input
               type="date"
+              id="enddate"
+              name="enddate"
+              value={medicationData.enddate}
+              onChange={handleChange}
               className=" w-full py-2 pl-2 rounded-md border border-black"
             />
           </div>
         </div>
-        <div className=" pt-5">
+        <div className="space-y-2 pt-5">
           <label htmlFor="">Time/Frequency</label> <br />
-          <div className=" pt-2">
-            <input type="radio" name="group" className=" mr-2" />
-            <label htmlFor="">Morning</label>
+          <div className="">
+            <input
+              type="checkbox"
+              name="morning"
+              id="morning"
+              checked={medicationData.timefrequency.includes('morning')}
+              onChange={handleChange}
+              className=" mr-2" 
+            />
+            <label htmlFor="morning">Morning</label>
           </div>
-          <div className=" pt-2">
-            <input type="radio" name="group" className=" mr-2" />
-            <label htmlFor="">Afternoon</label>
+          <div className="">
+            <input 
+              type="checkbox"
+              name="afternoon"
+              id="afternoon"
+              checked={medicationData.timefrequency.includes('afternoon')}
+              onChange={handleChange}
+              className=" mr-2" 
+            />
+            <label htmlFor="afternoon">Afternoon</label>
           </div>
-          <div className=" pt-2">
-            <input type="radio" name="group" className=" mr-2" />
-            <label htmlFor="">Evening</label>
+          <div className="">
+            <input 
+              type="checkbox"
+              name="evening"
+              id="evening"
+              checked={medicationData.timefrequency.includes('evening')}
+              onChange={handleChange}
+              className=" mr-2"
+            />
+            <label htmlFor="evening">Evening</label>
           </div>
         </div>
         <div className=" pt-5">
           <label htmlFor="">Note</label> <br />
           <textarea
-            name=""
-            id=""
+            name="note"
+            id="note"
+            value={medicationData.note}
+            onChange={handleChange}
             cols="30"
             rows="3"
             className=" w-full resize-none rounded-md mt-2 border border-black"
           ></textarea>
+        </div>
+        <div className=" pt-2">
+            <button type="submit" className=" py-2 px-4 bg-[#0A7DCF] text-white hover:bg-[#53D2E3] rounded-md">
+                Create Medication
+            </button>
         </div>
       </form>
     </div>
