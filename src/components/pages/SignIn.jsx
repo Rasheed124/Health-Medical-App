13416312cd68e19100943d8177825912a032283c
lord-space <teslimabdullah2@gmail.com>
2023-11-27 10:51:20 -0600

    Home Page Content , icons updated

--- src/components/pages/SignIn.jsx
+++ src/components/pages/SignIn.jsx
@@ -153,59 +153,7 @@ const SignIn = () => {
           </div>
         </main>
       </div>
-<<<<<<< HEAD
-
-      <form
-        onSubmit={handleSubmit}
-        className="w-full mx-auto mb-0 mt-8 max-w-md space-y-4"
-      >
-        {/* ... (same as your existing form structure) */}
-        {/* Add event handlers to update the state based on user input */}
-        <input
-          type="email"
-          required
-          className="w-full  rounded-lg  border border-black py-3 pl-3 pe-12 text-sm shadow-sm"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <input
-          type="password"
-          required
-          className="w-full rounded-lg border border-black py-3 pl-3 pe-12 text-sm shadow-sm"
-          placeholder="Enter password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">
-            Select User Type:
-          </label>
-          <select
-            className="w-full border border-black mt-1 p-2  rounded-md"
-            value={userType}
-            onChange={(e) => setUserType(e.target.value)}
-          >
-            <option value="patient">Patient</option>
-            <option value="doctor">Doctor</option>
-          </select>
-        </div>
-
-        <div className="flex items-center justify-center">
-          <button
-            type="submit"
-            className="block rounded-lg bg-blue-500 px-10 py-3 text-sm font-medium text-white"
-          >
-            Sign in
-          </button>
-        </div>
-      </form>
-    </div>
-=======
     </section>
->>>>>>> 2ad946ee8b5a8868344caf5a1762ecaa31e00ca4
   );
 };
 
